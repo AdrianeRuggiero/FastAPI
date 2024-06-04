a217bbe27e6a9cb726940824aa5932403cace9fd
AdrianeRuggiero <driketes@gmail.com>
2024-06-04 16:40:22 +0200

    creates tests

--- app/tests/test_math.py
+++ app/tests/test_math.py
@@ -1,3 +1,7 @@
-from app.controllers.math import inc
+from app.controllers.math import inc, minus
 
-print(inc(3))
+def test_inc():
+    assert inc(3) == 4
+
+def test_minus():
+    assert minus(3) == 2
